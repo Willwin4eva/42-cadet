499a7c2233c801290f6c8586d1abc9656acdb41e
Olbern Boothe <olboothe@e1z3r5p20.42.us.org>
2019-03-23 02:21:14 -0700

    libft

--- README.md
+++
@@ -1,3 +0,0 @@
-# libft
-Libft
-The first project at 42 we will be implementing many functions that we can call on to use going forward in the developer journey.
---
+++ ft_lstaddback.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstaddback.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olboothe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/20 13:44:54 by olboothe          #+#    #+#             */
+/*   Updated: 2019/03/20 14:05:36 by olboothe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstaddback(t_list **alst, t_list *new)
+{
+	t_list	*tmp;
+	t_list	*head;
+
+	tmp = *alst;
+	head = tmp;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	*alst = head;
+}
